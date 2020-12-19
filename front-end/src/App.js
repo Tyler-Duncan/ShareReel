91d2978c86092f249337dd352b3fbb2d7a6b596b
Tyler Duncan <tylerduncan365@yahoo.com>
2020-12-19 16:03:03 -0600

    Started creating front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+  <div className='default-style'>
+    <Container>
+      <h1>Share Reel</h1>
+    </Container>
+  </div>
+
+  );
+}
+
+export default App;
